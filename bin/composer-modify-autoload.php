a58715b6a20daf8292a24c5a5e97e39893685859
clgsru <clgsru@gmail.com>
2023-06-19 20:28:14 +0300

    Correct way

--- bin/composer-modify-autoload.php
+++ bin/composer-modify-autoload.php
@@ -3,7 +3,7 @@
  * Updates the vendor/autoload.php so it manually includes any files specified in composer.json's files array.
  * See https://github.com/composer/composer/issues/6768
  */
-$composer = json_decode(file_get_contents(__DIR__ . '/../../composer.json'));
+$composer = json_decode(file_get_contents(__DIR__ . '/../../../../composer.json'));
 
 const STRING_AUTOLOAD_REAL = "require_once __DIR__ . '/composer/autoload_real.php';";
 
@@ -20,10 +20,10 @@ foreach ($files as $f) {
 }
 
 // Read and re-write the vendor/autoload.php
-$autoload = file_get_contents(__DIR__ . '/vendor/autoload.php');
+$autoload = file_get_contents(__DIR__ . '/../../../autoload.php');
 $autoload = str_replace(
     STRING_AUTOLOAD_REAL,
     $patch_string . STRING_AUTOLOAD_REAL,
     $autoload);
 
-file_put_contents(__DIR__ . '/../autoload.php', $autoload);
+file_put_contents(__DIR__ . '/../../../autoload.php', $autoload);
